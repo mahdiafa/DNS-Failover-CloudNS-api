f4db3ea43a314cee1acbcf972392d826dfc2620f
Mahdi Afazeli <mahdi@afazeli.com>
2018-07-13 13:43:55 +0430

    add dir variable

--- web-avail-cloudns-api.sh
+++ web-avail-cloudns-api.sh
@@ -7,6 +7,7 @@
 
 time=`date '+%F %T'`
 logfile=/tmp/$0
+dir=$(dirname $0)
 recordcheckurl=https://api.cloudns.net/dns/records.json
 changerecordurl=https://api.cloudns.net/dns/change-record-status.json
 authid="your-authid" # change it to your authid
@@ -16,13 +17,13 @@ recordcheck=`curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass
 web=443
 
 #Remove old records
-rm -f `pwd`/records/* 
+rm -f $dir/records/* 
 
 #Get records and write it to the file with id number name
 for id in $(curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass&domain-name=$domain&host=&type=a" | jq -r .[].id); do echo $(curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass&domain-name=$domain&host=&type=a" | jq -r '."'$id'".record') > records/$id; done
 
 #Get IP address of the record
-for id in $(curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass&domain-name=$domain&host=&type=a" | jq -r .[].id); do ip=$(cat `pwd`/records/$id | grep $1); done
+for id in $(curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass&domain-name=$domain&host=&type=a" | jq -r .[].id); do ip=$(cat $dir/records/$id | grep $1); done
 
 #Find status of the record
 for id in $(curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass&domain-name=$domain&host=&type=a" | jq -r .[].id); do status=$(curl -s $recordcheckurl -d "auth-id=$authid&auth-password=$authpass&domain-name=$domain&host=&type=a" | jq -r '."'$id'".status'); done
