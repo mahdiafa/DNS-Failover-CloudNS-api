f41ef8dc3db3a7f7f8b4ac4cc4915980292af89e
Mahdi Afazeli <mahdi@afazeli.com>
2018-11-09 13:49:46 +0330

    add README

--- DNS-Failover-CloudNS-api.sh
+++ DNS-Failover-CloudNS-api.sh
@@ -4,9 +4,9 @@
 # Modify by: Mahdi Afazeli 2018-11-09
 # REV 1.0.B
 
-# Check the availabilty of website by ICMP and HTTP(S) to deactive unreachable host.
+# Check the availabilty of website by ICMP and HTTP(S) to deactive unreachable record.
 # This script check the A record that you entered in $3 argument to make sure your server with IP address ($1) is up and reachable.
-# also you can leave $3 alone if you have to check A records of yur domain without any subdomain name 
+# also you can leave $3 alone if you have to check A records of your domain without any subdomain name 
 # the server that you want to monitor must be pingable from where you run this script
 
 time=$(date '+%F %T')
---
+++ README.md
@@ -0,0 +1,4 @@
+Check the availabilty of website by ICMP and HTTP(S) to deactive unreachable record.
+This script check the A record that you entered in $3 argument to make sure your server with IP address ($1) is up and reachable.
+also you can leave $3 alone if you have to check A records of your domain without any subdomain name
+the server that you want to monitor must be pingable from where you run this script
